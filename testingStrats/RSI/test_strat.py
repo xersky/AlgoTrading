2b972a0a0386d1378d76b7f5e6d61fd2d5f9a6c1
xersky <ayoub.elaoufi@gmail.com>
2023-02-01 18:13:48 +0100

    Enhancing readability & quality of code
    - Logging the RSI value when trades are executed
    - Optimizing the calculation of output results
    - Cleaning Code

--- testingStrats/RSI/test_strat.py
+++ testingStrats/RSI/test_strat.py
@@ -40,8 +40,8 @@ for i in range(1, len(df)):
         btc_held += btc_to_buy
         # Record the trade
         trades.append(
-            {'timestamp': df.loc[i, 'timestamp'], 'type': 'buy', 'price': df.loc[i, 'close'], 'amount': btc_to_buy,
-             'balance': balance})
+            {'timestamp': df.loc[i, 'timestamp'], 'type': 'buy', 'price': df.loc[i, 'close'],
+             'rsi': df.loc[i, 'rsi'].__round__(2), 'amount': btc_to_buy, 'balance': balance})
 
     # Check if RSI is overbought (above RSI sell threshold) and we have some BTC
     if df.loc[i, 'rsi'] > rsi_sell_threshold and btc_held > 0:
@@ -53,63 +53,63 @@ for i in range(1, len(df)):
         btc_held = 0
         # Record the trade
         trades.append(
-            {'timestamp': df.loc[i, 'timestamp'], 'type': 'sell', 'price': df.loc[i, 'close'], 'amount': btc_to_buy,
-             'balance': balance})
+            {'timestamp': df.loc[i, 'timestamp'], 'type': 'sell', 'price': df.loc[i, 'close'],
+             'rsi': df.loc[i, 'rsi'].__round__(2), 'amount': btc_to_buy, 'balance': balance})
 
 # Convert the trades list into a DataFrame for analysis
 trades_df = pd.DataFrame(trades)
-max = 0
-min = 999999
-ATH = 0
-ATL = 999999
-date_1 = datetime
-date_2 = datetime
 
 with open("output.txt", "w") as txt_file:
     for item in trades:
         # write each item on a new line
         txt_file.write("%s\n" % item)
-        if item['balance'] > max:
-            max = item['balance']
-            date_2 = item['timestamp']
 
-        if 10 < item['balance'] < min:
-            min = item['balance']
-            date_1 = item['timestamp']
+# Retrieving the ATL_balance Balance, index and price
+ATL_balance = min(trades_df.iloc[1::2]['balance'])
+ATL_index = trades_df.iloc[1::2]['balance'].idxmin()
+ATL_price = trades[ATL_index]['price']
 
-        if item['price'] > ATH:
-            ATH = item['price']
+# ATH that comes after the ATL
+ATH_balance = max(trades_df.iloc[ATL_index::2]['balance'])
+ATH_index = trades_df.iloc[ATL_index::2]['balance'].idxmax()
+ATH_price = trades[ATH_index]['price']
 
-        if item['price'] < ATL:
-            ATL = item['price']
-
+# ATL Date
+ATL_date = trades[ATL_index]['timestamp']
+# ATH Date
+ATH_date = trades[ATH_index]['timestamp']
 
 print('RSI Range:')
 print('[ ', rsi_buy_threshold, ' - ', rsi_sell_threshold, ' ]')
 print()
 print('Portfolio Data:')
-print('Starting Balance: $', starting_balance, ' - at: ', datetime.fromtimestamp(trades[0]['timestamp'] / 1000).strftime("%d/%m/%Y %H:%M"))
-print('Portfolio ATL: $', min.__round__(2), ' - BTC Price: $', ATL, ' - at: ', datetime.fromtimestamp(date_1 / 1000).strftime("%d/%m/%Y %H:%M"))
-print('Portfolio ATH: $', max.__round__(2), ' - BTC Price: $', ATH, ' - at: ', datetime.fromtimestamp(date_2 / 1000).strftime("%d/%m/%Y %H:%M"))
-print('Final portfolio balance: $', trades[-1]['balance'].__round__(2), ' - BTC Price: $', trades[-1]['price'], ' - at: ', datetime.fromtimestamp(trades[-1]['timestamp'] / 1000).strftime("%d/%m/%Y %H:%M"))
+print('Starting Balance: $', starting_balance, ' - BTC Price: $', trades[0]['price'], ' - at: ',
+      datetime.fromtimestamp(trades[0]['timestamp'] / 1000).strftime("%d/%m/%Y %H:%M:%S"))
+print('Portfolio ATL: $', ATL_balance.__round__(2), ' - BTC Price: $', ATL_price, ' - at: ',
+      datetime.fromtimestamp(ATL_date / 1000).strftime("%d/%m/%Y %H:%M:%S"))
+print('Portfolio ATH: $', ATH_balance.__round__(2), ' - BTC Price: $', ATH_price, ' - at: ',
+      datetime.fromtimestamp(ATH_date / 1000).strftime("%d/%m/%Y %H:%M:%S"))
+print('Final portfolio balance: $', trades[-1]['balance'].__round__(2), ' - BTC Price: $', trades[-1]['price'],
+      ' - at: ', datetime.fromtimestamp(trades[-1]['timestamp'] / 1000).strftime("%d/%m/%Y %H:%M:%S"))
 print()
 print('RSI Strategy vs BTC: ')
 print('Balance if just held BTC: $', (trades[0]['amount'] * trades[-1]['price']).__round__(2))
-print('Balance if sold at ATH: $', (trades[0]['amount'] * ATH).__round__(2))
-print('Balance if bought at ATL and sold at ATH: $', ((starting_balance / ATL) * ATH).__round__(2))
+print('Balance if sold at ATH: $', (trades[0]['amount'] * ATH_price).__round__(2))
+print('Balance if bought at ATL and sold at ATH: $',
+      ((starting_balance / ATL_price) * ATH_price).__round__(2))
 
 # Calculate the cost of buying the bitcoins at each trade
 trades_df['time'] = pd.to_datetime(trades_df['timestamp'], unit='ms')
 trades_df['date'] = trades_df['time'].dt.date
 
-plt.subplot(1, 2, 1) # row 1, col 2 index 1
+plt.subplot(1, 2, 1)  # row 1, col 2 index 1
 plt.plot(trades_df['date'], trades_df['amount'])
 plt.title("Portfolio in BTC over time")
 plt.xlabel('Date')
 plt.ylabel('Balance in BTC')
 plt.xticks(rotation=90)
 
-plt.subplot(1, 2, 2) # index 2
+plt.subplot(1, 2, 2)  # index 2
 plt.plot(trades_df.iloc[1::2]['date'], trades_df.iloc[1::2]['balance'])
 plt.xlabel('Date')
 plt.ylabel('Balance $')
